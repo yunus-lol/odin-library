bf250d284d5ca79c02bb3674ea1689f39dc31571
Yunus Ahmed <YunusAhmed1266@gmail.com>
2025-09-02 11:42:30 +0100

    add delete book button for each card

--- script.js
+++ script.js
@@ -13,26 +13,56 @@ function addBookToLibrary(name, author, pages, read) {
   myLibrary.push(book);
 }
 
+let counter = 0
+
 function displayBooks() {
-  for (let x = 0; x < myLibrary.length; x++) {
+  for (counter; counter < myLibrary.length; counter++) {
     const card = document.querySelector(".card");
     const content = document.createElement("div");
     content.classList.add("card-content")
     content.innerHTML = `
-      <h3 class="book-name">${myLibrary[x].name}</h3>
-      <p class="book-info">Author: ${myLibrary[x].author}</p>
-      <p class="book-info">Pages: ${myLibrary[x].pages}</p>
-      <p class="book-info">Read: ${myLibrary[x].read}</p>
+      <h3 class="book-name">${myLibrary[counter].name}</h3>
+      <p class="book-info">Author: ${myLibrary[counter].author}</p>
+      <p class="book-info">Pages: ${myLibrary[counter].pages}</p>
+      <p class="book-info">Read: ${myLibrary[counter].read}</p>
     `
     card.appendChild(content)
   }
 }
 
-addBookToLibrary("bob", "jeff", "3", "read")
-addBookToLibrary("jeff", "jeff", "3", "not read")
-addBookToLibrary("bob", "jeff", "3", "read")
-addBookToLibrary("jeff", "jeff", "3", "read")
-addBookToLibrary("jeff", "jeff", "3", "read")
+addBookToLibrary("bob", "jeff", "3", "Read")
+addBookToLibrary("jeff", "jeff", "3", "Not read")
+addBookToLibrary("bob", "jeff", "3", "Read")
+addBookToLibrary("jeff", "jeff", "3", "Read")
+addBookToLibrary("jeff", "jeff", "3", "Read")
+
+console.log(myLibrary)
+
+const submitButton = document.querySelector(".submit");
+
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault()
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
+  const read = document.querySelector("#read").value;
+
+  dialog.close()
+  addBookToLibrary(title, author, pages, read);
+  displayBooks()
+});
 
 displayBooks()
-console.log(myLibrary)
+
+const body = document.querySelector("body");
+const dialog = document.querySelector("dialog");
+const addBook = document.querySelector(".addBook");
+const closeButton = document.querySelector(".close");
+
+addBook.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
