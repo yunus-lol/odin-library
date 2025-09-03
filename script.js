aba786f6d1502e28ed4c22db449e466a263423d1
Yunus Ahmed <YunusAhmed1266@gmail.com>
2025-09-03 21:23:38 +0100

    add toggle read button functionality

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ function Book(name, author, pages, read) {
   this.id = crypto.randomUUID();
 }
 
+Book.prototype.read = function() {
+  this.read = !this.read
+}
+
 function addBookToLibrary(name, author, pages, read) {
   const book = new Book(name, author, pages, read);
   myLibrary.push(book);
@@ -25,8 +29,9 @@ function displayBooks() {
       <h3 class="book-name">${myLibrary[counter].name}</h3>
       <p class="book-info">Author: ${myLibrary[counter].author}</p>
       <p class="book-info">Pages: ${myLibrary[counter].pages}</p>
-      <p class="book-info">Read: ${myLibrary[counter].read}</p>
+      <p class="book-info read">Read: ${myLibrary[counter].read}</p>
       <button class="deleteButton">Delete</button>
+      <button class="toggleButton">Toggle Read</button>
     `;
 
     const deleteButton = content.querySelector(".deleteButton")
@@ -37,6 +42,14 @@ function displayBooks() {
         content.remove();
         displayBooks();
       }
+    });
+
+    const toggleButton = content.querySelector(".toggleButton");
+    const read = content.querySelector(".read");
+    
+    toggleButton.addEventListener("click", () => {
+      const index = myLibrary.findIndex(book => book.read )
+      console.log(index)
     });
     
     card.appendChild(content)
@@ -53,7 +66,7 @@ addBookToLibrary("jeff", "jeff", "3", "Read")
 console.log(myLibrary)
 
 const submitButton = document.querySelector(".submit");
-submitButton.addEventListener("click", (event) => {
+submitButton.addEventListener("submit", (event) => {
   event.preventDefault()
   const title = document.querySelector("#title").value;
   const author = document.querySelector("#author").value;
