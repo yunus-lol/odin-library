d8f61baf53619e3dae6313dcdc390d7c538088aa
Yunus Ahmed <YunusAhmed1266@gmail.com>
2025-09-05 21:07:12 +0100

    debug the changing read status

--- script.js
+++ script.js
@@ -8,77 +8,76 @@ function Book(name, author, pages, read) {
   this.id = crypto.randomUUID();
 }
 
-Book.prototype.read = function() {
-  this.read = !this.read
+Book.prototype.toggleRead = function() {
+  this.read = !this.read;
 }
 
 function addBookToLibrary(name, author, pages, read) {
   const book = new Book(name, author, pages, read);
   myLibrary.push(book);
+  displayBooks();
 }
 
-let counter = 0
+let counter = 0;
 
 function displayBooks() {
+  const card = document.querySelector(".card");
   for (counter; counter < myLibrary.length; counter++) {
-    const card = document.querySelector(".card");
     const content = document.createElement("div");
-    content.classList.add("card-content")
+    content.classList.add("card-content");
     content.dataset.id = myLibrary[counter].id;
     content.innerHTML = `
       <h3 class="book-name">${myLibrary[counter].name}</h3>
       <p class="book-info">Author: ${myLibrary[counter].author}</p>
       <p class="book-info">Pages: ${myLibrary[counter].pages}</p>
-      <p class="book-info read">Read: ${myLibrary[counter].read}</p>
+      <p class="book-info">Read: ${myLibrary[counter].read ? "Read" : "Not read"}</p>
       <button class="deleteButton">Delete</button>
       <button class="toggleButton">Toggle Read</button>
     `;
 
-    const deleteButton = content.querySelector(".deleteButton")
+    const deleteButton = content.querySelector(".deleteButton");
+
     deleteButton.addEventListener("click", () => {
-      const index = myLibrary.findIndex(book => book.id === content.getAttribute("data-id"));
-      if (index !== -1) {
-        myLibrary.splice(index, 1);
-        content.remove();
-        displayBooks();
+      for (let x = 0; x < myLibrary.length; x++) {
+        if (myLibrary[x].id === content.getAttribute("data-id")) {
+          myLibrary.splice(myLibrary[x], 1);
+          content.remove();
+          displayBooks();
+        }
       }
     });
 
     const toggleButton = content.querySelector(".toggleButton");
-    const read = content.querySelector(".read");
-    
+
     toggleButton.addEventListener("click", () => {
-      const index = myLibrary.findIndex(book => book.read )
-      console.log(index)
+     for (let x = 0; x < myLibrary.length; x++) {
+        if (myLibrary[x].id === content.getAttribute("data-id")) {
+          myLibrary[x].toggleRead();
+          displayBooks();
+        }
+      }
     });
-    
-    card.appendChild(content)
+
+    card.appendChild(content);
   };
 }
 
-
-addBookToLibrary("bob", "jeff", "3", "Read")
-addBookToLibrary("jeff", "jeff", "3", "Not read")
-addBookToLibrary("bob", "jeff", "3", "Read")
-addBookToLibrary("jeff", "jeff", "3", "Read")
-addBookToLibrary("jeff", "jeff", "3", "Read")
-
-console.log(myLibrary)
+console.log(myLibrary);
 
 const submitButton = document.querySelector(".submit");
 submitButton.addEventListener("submit", (event) => {
-  event.preventDefault()
+  event.preventDefault();
   const title = document.querySelector("#title").value;
   const author = document.querySelector("#author").value;
   const pages = document.querySelector("#pages").value;
   const read = document.querySelector("#read").value;
 
-  dialog.close()
+  dialog.close();
   addBookToLibrary(title, author, pages, read);
-  displayBooks()
+  displayBooks();
 });
 
-displayBooks()
+displayBooks();
 
 const body = document.querySelector("body");
 const dialog = document.querySelector("dialog");
@@ -92,3 +91,9 @@ addBook.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
   dialog.close();
 });
+
+addBookToLibrary("bob", "jeff", "3", true);
+addBookToLibrary("jeff", "jeff", "3", false);
+addBookToLibrary("bob", "jeff", "3", true);
+addBookToLibrary("jeff", "jeff", "3", false);
+addBookToLibrary("jeff", "jeff", "3", true);
